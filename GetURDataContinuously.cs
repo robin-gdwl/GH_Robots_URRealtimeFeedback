beace791e1ff3882fd546bee17116b595eabbec9
Robin Godwyll <rodwyll+github@gmail.com>
2021-02-11 22:43:34 +0100

    Initial Files

---
+++ GetURDataContinuously.cs
@@ -0,0 +1,128 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using Rhino;
+using Rhino.Geometry;
+
+using Grasshopper;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Data;
+using Grasshopper.Kernel.Types;
+
+using Grasshopper.Kernel.Special;
+using Robots;
+
+
+
+/// <summary>
+/// This class will be instantiated on demand by the Script component.
+/// </summary>
+public class Script_Instance : GH_ScriptInstance
+{
+#region Utility functions
+  /// <summary>Print a String to the [Out] Parameter of the Script component.</summary>
+  /// <param name="text">String to print.</param>
+  private void Print(string text) { /* Implementation hidden. */ }
+  /// <summary>Print a formatted String to the [Out] Parameter of the Script component.</summary>
+  /// <param name="format">String format.</param>
+  /// <param name="args">Formatting parameters.</param>
+  private void Print(string format, params object[] args) { /* Implementation hidden. */ }
+  /// <summary>Print useful information about an object instance to the [Out] Parameter of the Script component. </summary>
+  /// <param name="obj">Object instance to parse.</param>
+  private void Reflect(object obj) { /* Implementation hidden. */ }
+  /// <summary>Print the signatures of all the overloads of a specific method to the [Out] Parameter of the Script component. </summary>
+  /// <param name="obj">Object instance to parse.</param>
+  private void Reflect(object obj, string method_name) { /* Implementation hidden. */ }
+#endregion
+
+#region Members
+  /// <summary>Gets the current Rhino document.</summary>
+  private readonly RhinoDoc RhinoDocument;
+  /// <summary>Gets the Grasshopper document that owns this script.</summary>
+  private readonly GH_Document GrasshopperDocument;
+  /// <summary>Gets the Grasshopper script component that owns this script.</summary>
+  private readonly IGH_Component Component;
+  /// <summary>
+  /// Gets the current iteration count. The first call to RunScript() is associated with Iteration==0.
+  /// Any subsequent call within the same solution will increment the Iteration count.
+  /// </summary>
+  private readonly int Iteration;
+#endregion
+
+  /// <summary>
+  /// This procedure contains the user code. Input parameters are provided as regular arguments,
+  /// Output parameters as ref arguments. You don't have to assign output parameters,
+  /// they will have a default value.
+  /// </summary>
+  private void RunScript(bool AutoUpdate, string IP, ref object A, ref object B)
+  {
+    datapack = new List<double>();
+
+    if (RobotConnection == null)
+    {
+      RobotAddress = IP;
+      RobotConnection = new URRealTime(RobotAddress);
+      Feedbackdata = RobotConnection.FeedbackData;
+    }
+
+    if (DataAsTree == null)
+    {
+      DataAsTree = new DataTree<double>();
+    }
+
+    if (task == null || task.IsCompleted)
+    {
+      task = System.Threading.Tasks.Task.Run(() => Animation());
+    }
+
+    A = DataAsTree;
+    if (AutoUpdate == true)
+    {
+      Component.ExpireSolution(true);
+    }
+
+  }
+
+  // <Custom additional code> 
+  System.Threading.Tasks.Task task = null;
+  URRealTime RobotConnection;
+  String RobotAddress;
+  DataTree<double> DataAsTree;
+  List<double> datapack;
+  List<FeedbackType> Feedbackdata;
+  GH_Path path;
+
+  void Animation()
+  {
+    RobotConnection.UpdateFeedback();
+    Feedbackdata = RobotConnection.FeedbackData;
+
+    for (var j = 0; j < Feedbackdata.Count; j++)
+    {
+      datapack = new List<double>();
+      for (var k = 0; k < Feedbackdata[j].Value.Length; k++)
+      {
+        datapack.Add(Feedbackdata[j].Value[k]);
+      }
+      path = new GH_Path(j);
+      DataAsTree.RemovePath(j);
+      DataAsTree.AddRange(datapack, path);
+      DataAsTree.TrimExcess();
+    }
+  }
+
+
+  /// <summary>
+  /// This method will be called once every solution, before any calls to RunScript.
+  /// </summary>
+  public override void BeforeRunScript()
+  { }
+  /// <summary>
+  /// This method will be called once every solution, after any calls to RunScript.
+  /// </summary>
+  public override void AfterRunScript()
+  { }
+
+  // </Custom additional code> 
+}
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # GH_Robots_URRealtimeFeedback
- Realtime Feedback from Universal Robots in Grasshopper. uses the "Robots" Plugin.
+ Realtime Feedback from Universal Robots in Grasshopper. uses the [Robots Plugin](https://github.com/visose/Robots/).
+
+ C# code based on [this comment.](https://github.com/visose/Robots/issues/19#issuecomment-520556168)
+
+![UR Realtime Feedback01](https://media3.giphy.com/media/EjhP0Xyx0qNc8hxUZq/giphy.gif)
+
+Demonstration Video: 
+https://youtu.be/c1BiL6fXkgs 
+
+
+## Grasshopper Canvas:
+
+![UR Realtime GH-Canvas](UR-Robotfeedback01_Canvas01.png)
+
+## Usage: 
+Tested with Rhino 6 on Windows. 
+On opening the Grasshopper definition you may be prompted to specify the location of the **Robots.DLL** file. It is located in your grasshopper Library driectory (where you install plugins)
+
+There may be an issue with the C# script consuming uneccesary amounts of RAM. I hope to fix this soon. 
+
+# TODO:
+- Test on mac 
+- Investigate Memory Leak
+- Annotate GH Definition 
+- Comment C#-script code
+- make explanation Video 
